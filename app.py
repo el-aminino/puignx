f79604ba1c54ebe860897f3f6365ed50b3c6fb33
el-aminino <aminmze81@outlook.com>
2024-01-01 01:13:36 +0330

    some added files

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask , render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
-
+    return "Hello World!"
 
 if __name__== '__main__':
     app.run(debug=True)
