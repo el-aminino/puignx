701942b1dd27b2f42e85cead55cceecc9876d0ef
el-aminino <aminmze81@gmail.com>
2024-01-01 08:25:21 +0330

    commit for se

--- defs/sites-enabled.py
+++ defs/sites-enabled.py
@@ -1,4 +1,14 @@
 import os
 config_path = '/etc/nginx/sites-enabled/'
 listdir = os.listdir(config_path)
-print(listdir)
+
+for i in listdir :
+    file = config_path+i
+    with open(file,"r") as f :
+        if "server_name" in f.readline():
+            print(f.readline)
+
+
+
+
+#print(listdir)
