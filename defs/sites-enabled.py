5173bf200adf372ec3b36d6eb026ebd126a8066d
el-aminino <aminmze81@gmail.com>
2024-01-01 08:28:40 +0330

    e

--- defs/sites-enabled.py
+++ defs/sites-enabled.py
@@ -4,10 +4,10 @@ listdir = os.listdir(config_path)
 
 for i in listdir :
     file = config_path+i
-    print(file)
+    
     with open(file,"r") as f :
         if "server_name" in f.readline():
-            print(f.readline)
+            print(type(f.readline))
 
 
 
